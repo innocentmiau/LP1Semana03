abc773608f3d55265b96d9f7eca00ad7817fce33
André <imandreleandro@gmail.com>
2025-03-05 14:18:29 +0000

    Added RightSize project

---
+++ RightSize/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string s in args)
+            {
+                if (s.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                if (s.Length > 3) Console.WriteLine(s);
+            }
+        }
+    }
+}
