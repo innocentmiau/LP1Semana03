12a9fd1496a6c1617b42b951aa267ee574840f9f
André <imandreleandro@gmail.com>
2025-03-05 14:35:06 +0000

    Added ChangeString project

---
+++ ChangeString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Digite um texto:");
+            string input = Console.ReadLine();
+            Console.Write("Digite um caracter:");
+            char input2 = char.Parse(Console.ReadLine());
+            string text = "";
+            for (int i = 0; i<input.Length; i++)
+            {
+                char toAdd = input[i] == input2 ? 'x' : input[i];
+                text += toAdd;
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
