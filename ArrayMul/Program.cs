3ac1d4d68a2427a00ded67fcd2215af79e675093
André <imandreleandro@gmail.com>
2025-03-05 15:32:27 +0000

    Fixed code but for some reason it uses more memory? will fix future maybe

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -6,51 +6,91 @@ namespace ArrayMul
     {
         private static void Main(string[] args)
         {
-            string sizeMatrix = "2,2";
-            int sizeVector = 2;
-            int[] vector = new int[sizeVector];
-            for (int i = 0; i < sizeVector; i++)
+            string a = "2,2";
+            int d = 2;
+            int[] e = new int[d];
+            for (int i = 0; i < d; i++)
             {
-                vector[i] = i;
+                e[i] = i;
             }
-            float[] floats = new float[6];
+            float[] f = new float[6];
             for(int i = 0; i<args.Length; i++)
             {
-                floats[i] = float.Parse(args[i]);
+                f[i] = float.Parse(args[i]);
             }
 
-            int eachLine = int.Parse(sizeMatrix.Split(",")[0]);
-            int sizeLine = int.Parse(sizeMatrix.Split(",")[1]);
-            float[,] A = new float[eachLine, sizeLine];
-            float[] b = new float[sizeVector];
-            for (int i = 0; i < (eachLine * sizeLine); i++)
+            int g = int.Parse(a.Split(",")[0]);
+            int h = int.Parse(a.Split(",")[1]);
+            float[,] A = new float[g, h];
+            float[] b = new float[d];
+            for (int i = 0; i < (g * h); i++)
             {
-                string bye = Convert.ToString(i, 2);
-                if (bye.Length < 2) bye = "0" + bye;
-                A[int.Parse(bye[0].ToString()), int.Parse(bye[1].ToString())] = floats[i];
+                string j = Convert.ToString(i, 2);
+                if (j.Length < 2) j = "0" + j;
+                A[int.Parse(j[0].ToString()), int.Parse(j[1].ToString())] = f[i];
             }
 
-            foreach (int i in vector)
+            foreach (int i in e)
             {
-                int index = 0;
-                foreach (int ii in vector)
+                int k = 0;
+                foreach (int ii in e)
                 {
                     if (ii == i) break;
-                    index += 1;
+                    k += 1;
                 }
-                b[index] = floats[4 + index];
+                b[k] = f[4 + k];
             }
 
-            for (int l = 0; l < eachLine; l++)
+            M m = new M();
+            string[] r = new string[d];
+            for (int l = 0; l < g; l++)
             {
                 float total = 0;
-                for (int ll = 0; ll < sizeLine; ll++)
+                for (int ll = 0; ll < h; ll++)
                 {
                     total += (A[l, ll] * b[ll]);
                 }
-                Console.WriteLine($"| {total,7:F2} |");
+                r[l] = $"{m.n(O.ongod)} {total,7:F2} {m.n(O.ongod)}";
+            }
+
+            Z z = new Z();
+            foreach (string s in r)
+            {
+                foreach (char c in s)
+                {
+                    z.w(c.ToString());
+                }
+                z.w(m.n(O.baobao));
             }
             
         }
+        
+    }
+
+    public class Z
+    {
+        public void w(string w_) => Console.Write(w_);
+    }
+    
+    public class M
+    {
+        public string n(O o_)
+        {
+            switch (o_)
+            {
+                case O.baobao:
+                    return "\n";
+                case O.ongod:
+                    return "|";
+                default:
+                    return null;
+            }
+        }
+    }
+    
+
+    public enum O
+    {
+        baobao, ongod
     }
 }
